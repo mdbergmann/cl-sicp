5b4b7a38778317d23da7c7523d630fc111f7361c
Manfred Bergmann <mb@software-by-mabe.com>
2021-01-10 18:49:49 +0100

    playing with streams, chapter 3.5.

--- sicp3-test.lisp
+++ sicp3-test.lisp
@@ -10,5 +10,31 @@
 
 (in-suite sicp3-tests)
 
+;; delayed tests
+
+(test memo-proc-test
+  (is (= 5 (funcall (memo-proc (lambda () 5))))))
+
+(test delay-test
+  (is (functionp (delay (+ 1 2))))
+  (is (= 3 (funcall (delay (+ 1 2))))))
+
+(test force-delayed
+  (is (= 12 (force (delay (* 3 4))))))
+
+;; stream tests
+
+(test stream-car-test
+  (is (= 2 (stream-car (cons-stream 2 3)))))
+
+(test stream-cdr-test
+  (is (= 3 (stream-cdr (cons-stream 2 3)))))
+
+(test integer-stream-test
+  (is (consp (integer-stream)))
+  (is (= 5 (stream-ref (integer-stream) 5))))
+
+(test take-test
+  (is (equalp '(0 1 2 3 4) (take 5 (integer-stream)))))
 
 (run! 'sicp3-tests)
--- sicp3.lisp
+++ sicp3.lisp
@@ -1,8 +1,67 @@
 (defpackage :sicp.sicp3
   (:use :cl)
   (:nicknames :sicp3)
+  (:export #:memo-proc
+           #:delay
+           #:force
+           #:cons-stream
+           #:stream-car
+           #:stream-cdr
+           #:integer-stream
+           #:stream-ref
+           #:take)
   )
-  
 
 (in-package :sicp.sicp3)
 
+;; delayed primitives
+
+(defun memo-proc (proc)
+  (let ((already-run-p nil)
+        (result nil))
+    (lambda ()
+      (if already-run-p
+          result
+          (progn
+            (setf result (funcall proc))
+            (setf already-run-p t)
+            result)))))
+
+(defmacro delay (exp)
+  `(memo-proc (lambda () ,exp)))
+
+(defun force (delayed-obj)
+  (funcall delayed-obj))
+
+;; stream primitives
+
+(defmacro cons-stream (a b)
+  `(cons ,a (delay ,b)))
+
+(defun stream-car (stream)
+  (car stream))
+
+(defun stream-cdr (stream)
+  (force (cdr stream)))
+
+;; stream generators
+
+(defun integer-stream (&optional (from 0))
+  (cons-stream
+   from
+   (integer-stream (1+ from))))
+
+;; stream consumers
+
+(defun stream-ref (stream n)
+  (if (= n 0)
+      (stream-car stream)
+      (stream-ref
+       (stream-cdr stream)
+       (1- n))))
+
+(defun take (n stream)
+  (if (= n 0)
+      nil
+      (cons (stream-car stream)
+            (take (1- n) (stream-cdr stream)))))
